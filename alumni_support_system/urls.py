fc30c37fab432b9f9841ab96067bd3e5ed9df003
anmoljindal <anmoljindal.in@gmail.com>
2017-04-06 04:53:09 +0530

    first commit

---
+++ alumni_support_system/urls.py
@@ -0,0 +1,49 @@
+"""
+Definition of urls for alumni_support_system.
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.conf.urls import include, url
+from django.contrib.auth import views
+from core import views as core_views
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', alumni_support_system.views.home, name='home'),
+    # url(r'^alumni_support_system/', include('alumni_support_system.alumni_support_system.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^$', core_views.home, name = 'home'),
+    url(r'^login/$', views.login),
+    url(r'^logout/$', views.logout, {'next_page' : '/login'}),
+    url(r'^login/register/$', core_views.register_page),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    url(r'^profile/$', core_views.view_profile, name = 'view_profile'),
+    url(r'^profile/editprofile/$', core_views.update_profile, name='edit_profile'),
+    url(r'^profile/writetestimonial/$', core_views.write_testimonial, name='write_testimonial'),
+    url(r'^profile/viewtestimonial/$', core_views.view_testimonial, name='view_testimonial'),
+    url(r'^profile/deletetestimonial/$', core_views.delete_testimonial, name='delete_testimonial'),
+    url(r'^settings/$', core_views.settings, name='settings'), 
+    url(r'^settings/changepassword/$', core_views.change_password, name='change_password'),
+    url(r'^settings/deleteuser/$', core_views.delete_user, name='delete_user'),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+     
---
+++ core/views.py
@@ -0,0 +1,159 @@
+from django.template import RequestContext
+from django.shortcuts import render,redirect 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib import messages
+from django.db import transaction
+from core.forms import *
+
+#to login
+@login_required(login_url='/login')
+def home(request):
+    return render(request, 'home.html') 
+
+#user registration
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(username=form.cleaned_data['username'],password=form.cleaned_data['password1'],email=form.cleaned_data['email'])
+            messages.success(request, "Registration Successful")
+            return redirect('/login') 
+        else:
+           messages.error(request, "Please correct the error below")
+           return render(request,'registration/register.html',{'form':form})
+          
+    else:
+        form = RegistrationForm()
+        return render(request,'registration/register.html',{'form':form})
+
+#viewing user profile
+@login_required(login_url='/login')
+def view_profile(request):
+    return render(request, 'profiles/view_profile.html')
+
+
+#updating user profile
+@login_required(login_url='/login')
+@transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request,'Your profile was successfully updated!')
+            return redirect('/profile')
+        else:
+            messages.error(request, ('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'profiles/edit_profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
+
+
+#viewing user settings
+@login_required(login_url='/login')
+def settings(request):
+    return render(request, 'settings/settings.html')
+
+
+#change password functionality
+@login_required(login_url='/login')
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # to update session data to prevent user from logging out
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('/')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'settings/change_password.html', {'form': form})
+
+ 
+#delete account
+@login_required(login_url='/login')
+def delete_user(request):
+    if request.method == 'POST':
+        form = DeleteUserForm(request.POST)
+        if form.is_valid():
+           cuser = request.user
+           try:
+                user = User.objects.get(username = form.cleaned_data['username'])
+                if user.username == cuser.username:                                   
+                    user.delete()
+                    messages.success(request, 'Your account has been deleted successfully. Looking forward to having you back again')
+                    return redirect('/login')  
+                else:
+                    messages.error(request, 'You are not authorised to perform this operation') 
+           except ObjectDoesNotExist:
+               user = None
+               messages.error(request,'Invalid Username')           
+        else:
+            messages.error(request, 'Please correct the error below')
+    else:
+        form = DeleteUserForm()
+    return render(request, 'settings/delete_user.html', {'form' : form}) 
+
+
+#writing testimonial
+@login_required(login_url='/login')
+@transaction.atomic
+def write_testimonial(request):
+    if request.method == 'GET':
+         try:
+            testimonial = Testimonial.objects.get(user = request.user)
+            messages.success(request, 'You have already written a testimonial')
+            return render(request,'profiles/view_testimonial.html', {'testimonial': testimonial})
+         except ObjectDoesNotExist:
+            testimonial_form = TestimonialForm()
+            return render(request, 'profiles/write_testimonial.html', {'testimonial_form':testimonial_form})
+
+    if request.method == 'POST':
+        testimonial_form = TestimonialForm(request.POST)
+        if testimonial_form.is_valid():
+            testimonial = testimonial_form.save(commit = False)
+            testimonial.user = request.user
+            testimonial.save()
+            messages.success(request,'Thanks for writing')
+            return redirect('/profile')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+       testimonial_form = TestimonialForm()
+    return render(request, 'profiles/write_testimonial.html', {
+        'testimonial_form' : testimonial_form
+    })
+
+#viewing testimonial
+@login_required(login_url='/login')
+def view_testimonial(request):
+    if request.method == 'GET':    
+        try:        
+            testimonial = Testimonial.objects.get(user = request.user)
+            return render(request,'profiles/view_testimonial.html', {'testimonial': testimonial})
+        except ObjectDoesNotExist:
+            messages.error(request,'You have not written a testimonial')
+            return render(request, 'profiles/view_profile.html')
+
+#deleting testimonial
+@login_required(login_url='/login')
+def delete_testimonial(request):
+    if request.method == 'GET':
+        try :
+            testimonial = Testimonial.objects.get(user = request.user)
+            testimonial.delete()
+            messages.success(request,'Testimonial deleted successfully')
+            return render(request,'profiles/view_profile.html') 
+        except ObjectDoesNotExist:
+            messages.error(request, 'You have not written a testimonial')
+            return render(request,'profiles/view_profile.html') 
